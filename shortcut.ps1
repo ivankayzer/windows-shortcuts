30891f1e4d65ac66402f3a3c46526a1c14655793
Ivan Kayzer <ivan.kayzer@outlook.com>
2024-05-12 16:37:07 +0200

    Update shortcut.ps1

--- shortcut.ps1
+++ shortcut.ps1
@@ -3,7 +3,7 @@ $Action = $args[0]
 Register-ScheduledTask -TaskName "Shortcut Action" -InputObject (
   (
     New-ScheduledTask -Action (
-             (New-ScheduledTaskAction -Execute "$PsHome\powershell.exe" -Argument "-Command `"Start-Process -Verb RunAs powershell '-ExecutionPolicy Bypass -Command cd \\\`"$(Get-Location)\\\`"; & \\\`".\run.ps1\\\`" $Action' `"")
+             (New-ScheduledTaskAction -Execute "$PsHome\powershell.exe" -Argument "-Command `"Start-Process -Verb RunAs powershell '-ExecutionPolicy Bypass -Command cd \\\`"$(Split-Path -Path $MyInvocation.MyCommand.Path -Parent)\\\`"; & \\\`".\run.ps1\\\`" $Action' `"")
     ) -Trigger (
       New-ScheduledTaskTrigger -Once -At 3am
     ) -Principal ( New-ScheduledTaskPrincipal -UserId "$Env:ComputerName\$Env:UserName" -LogonType ServiceAccount -RunLevel Highest )
