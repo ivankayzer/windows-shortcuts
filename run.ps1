989c052afa9c9ac4759dd47bdc0dabc820d4dd9a
Ivan Kayzer <ivan.kayzer@outlook.com>
2024-06-24 20:49:27 +0200

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,39 +1,69 @@
 $Action = $args[0]
 
-switch ($Action) {
-  "60hz" { C:\rtss-cli.exe limit:set 60 }
-  "120hz" { C:\rtss-cli.exe limit:set 120 }
-  "steam" {
-    Start-Process C:\"Program Files (x86)"\Steam\steam.exe
-    C:\nircmd.exe win close title "Steam Big Picture Mode"
+Function Run {
+  param($action)
+
+  Function FPS {
+    param($fps)
+    C:\rtss-cli.exe limit:set $fps
   }
-  "tv_mode" {
-    DisplaySwitch 4
-    Start-Process C:\Users\ivank\Documents\Scripts\toggle_hdr_on.bat
-    C:\rtss-cli.exe limit:set 120
+
+  Function Resolution4k {
+    param($index)
+    C:\ChangeScreenResolution.exe /w=3840 /h=2160 /d=$index
   }
-  "monitor_mode" {
-    DisplaySwitch 1
-    Start-Process C:\Users\ivank\Documents\Scripts\toggle_hdr_off.bat
-    C:\rtss-cli.exe limit:set 60
+
+  Function Resolution800p {
+    C:\ChangeScreenResolution.exe /w=1920 /h=1200 /d=0
   }
-  "turn_pc_off" {
-    shutdown /s /t 0
+
+  switch ($action) {
+    "60hz" { FPS "60" }
+    "120hz" { FPS "120" }
+    "90hz" { FPS "90" }
+    "steam" {
+      Start-Process C:\"Program Files (x86)"\Steam\steam.exe
+      C:\nircmd.exe win close title "Steam Big Picture Mode"
+    }
+    "tv_mode" {
+      DisplaySwitch 4
+      Start-Process C:\Users\ivank\Documents\Scripts\toggle_hdr_on.bat
+      Run "120hz"
+      Resolution4k 1
+    }
+    "monitor_mode" {
+      DisplaySwitch 1
+      Start-Process C:\Users\ivank\Documents\Scripts\toggle_hdr_off.bat
+      Run "60hz"
+      Resolution4k 0
+    }
+    "steam_deck_mode" {
+      Run "monitor_mode"
+      Run "90hz"
+      Resolution800p
+    }
+    "turn_pc_off" {
+      shutdown /s /t 0
+    }
+    "toggle_capframe" {
+      # Requires CapFrameX to run
+      Start-Process C:\Users\ivank\Documents\Scripts\f1.exe
+    }
+    "steam_big_picture" {
+      Start-Process C:\"Program Files (x86)"\Steam\steam.exe
+      Start-Process "steam://open/bigpicture"
+    }
+    "playnite" {
+      Start-Process C:\Users\ivank\AppData\Local\Playnite\Playnite.toDesktopApp.exe
+    }
+    "playnite_fs" {
+      Start-Process -FilePath "C:\Users\ivank\AppData\Local\Playnite\Playnite.DesktopApp.exe" -ArgumentList "--startfullscreen"
+    }
   }
-  "toggle_capframe" {
-    # Requires CapFrameX to run
-    Start-Process C:\Users\ivank\Documents\Scripts\f1.exe
-  }
-  "steam_big_picture" {
-    Start-Process C:\"Program Files (x86)"\Steam\steam.exe
-    Start-Process "steam://open/bigpicture"
-  }
-  "playnite" {
-    Start-Process C:\Users\ivank\AppData\Local\Playnite\Playnite.DesktopApp.exe
-  }
-  "playnite_fs" {
-    Start-Process -FilePath "C:\Users\ivank\AppData\Local\Playnite\Playnite.DesktopApp.exe" -ArgumentList "--startfullscreen"
-  }
+  
+  Unregister-ScheduledTask -TaskName "Shortcut Action" -Confirm:$false -ErrorAction SilentlyContinue
 }
 
-Unregister-ScheduledTask -TaskName "Shortcut Action" -Confirm:$false
+Run $Action
+
+
